886166c113cce02ae1a93e756b5c4ca59a82d806
TejasShirsath <tejasshirsath5@gmail.com>
2025-02-18 03:30:55 +0530

    Add Hero component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import Hero from './components/Hero';
 
 function App() {
   return (
-    <>
-    <h1>Hello</h1>
-    </>
+    <div className="App">
+      <Hero />
+    </div>
   );
 }
 
